ce37d44c30637ebe741b2d40fe2bcfd921f1b6f2
zebulgado <othon.negromonte@gmail.com>
2021-01-05 08:31:20 -0300

    Starting project

---
+++ src/model/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  address: {
+      type: String,
+      required: true,
+
+      state: {
+          type: String,
+          required: true,
+          uppercase: true
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      neighborhood: {
+        type: String,
+        required: true
+      },
+      number: {
+          type: Number,
+          required: true
+      },
+      complement: {
+          type: String,
+          required: false
+      }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+// export model user with UserSchema
+module.exports = mongoose.model("User", UserSchema);
